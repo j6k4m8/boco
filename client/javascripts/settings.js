c5537d7137ce754610cc9d74880eba60a30f3b07
Jordan <j6k4m8@gmail.com>
2015-06-03 12:58:47 -0400

    Add expiry and plot saving

--- client/javascripts/settings.js
+++ client/javascripts/settings.js
@@ -18,5 +18,34 @@ Template.settingsPane.events({
             minSpareRows: 1,
             stretchH: 'all'
         });
+    },
+
+    'click .js-link-trigger': function() {
+        var _data = spreadsheet.data().handsontable.getData();
+        var _plotOptions = PlotOptions;
+
+        Meteor.call('getNewLink', {
+            data: _data,
+            plotOptions: _plotOptions
+        }, function(err, val) {
+            if (!err) {
+                prompt('Use this link to navigate directly back to this graph.', 'http://boco.jordan.matelsky.com/plots/' + val);
+            }
+        });
     }
 });
+
+
+Template.settingsPane.helpers({
+    willExpire: function() {
+        return !!Session.get('page_plot')
+    },
+
+    plot_date: function() {
+        return moment(Session.get('page_plot').created).format('LLLL')
+    },
+
+    plot_expiry: function() {
+        return moment(Session.get('page_plot').created).add(1, 'w').calendar()
+    }
+})
--- client/javascripts/ui.js
+++ client/javascripts/ui.js
@@ -1,17 +1,38 @@
-Template.home.rendered = function() {
-    spreadsheet = $("#spreadsheet").handsontable({
-        data: data100,
-        manualColumnResize: true,
-        rowHeaders: true,
-        colHeaders: true,
-        minSpareRows: 1,
-        stretchH: 'all',
+Template.home.created = function() {
+    if (!!this.data.plot_uuid) {
+        Session.set('waitingForPlotData', true);
+        Meteor.call('getPlot', this.data.plot_uuid, function(err, val) {
+            if (!err) {
+                Session.set('page_plot', val)
+                Session.set('waitingForPlotData', false);
+            }
+        });
+    }
+};
 
-        // afterChange: function() {
-        //     $('#scatter-load').html('');
-        //     showScatterPlot(hotParseToJSON());
-        // }
-    });
+
+Template.home.rendered = function() {
+    if (Session.get('waitingForPlotData') == true) {
+        Meteor.setTimeout(function() {
+            spreadsheet = $("#spreadsheet").handsontable({
+                data: Session.get('page_plot').data,
+                manualColumnResize: true,
+                rowHeaders: true,
+                colHeaders: true,
+                minSpareRows: 1,
+                stretchH: 'all'
+            });
+        }, 1000);
+    } else {
+        spreadsheet = $("#spreadsheet").handsontable({
+            data: data100,
+            manualColumnResize: true,
+            rowHeaders: true,
+            colHeaders: true,
+            minSpareRows: 1,
+            stretchH: 'all'
+        });
+    }
 
     $('.js-hot-toggle').click(function() {
         $('.hot-pane').toggleClass('hidden');
